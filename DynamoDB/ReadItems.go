f7affe31c1adf84d1ce44d99bc60a99098c86d27
Jodiann Henry <109822667+jojohenry@users.noreply.github.com>
2023-04-20 12:58:55 -0400

    Create ReadItems.go

---
+++ DynamoDB/ReadItems.go
@@ -0,0 +1,70 @@
+package main
+
+// snippet-start:[dynamodb.go.read_item.imports]
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
+	"fmt"
+	"log"
+)
+
+// Create struct to hold info about new item
+type Item struct {
+	Year   int
+	Title  string
+	Plot   string
+	Rating float64
+}
+
+// snippet-end:[dynamodb.go.read_item.struct]
+
+func main() {
+
+	// and region from the shared configuration file ~/.aws/config.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+	// snippet-end:[dynamodb.go.read_item.session]
+
+	// snippet-start:[dynamodb.go.read_item.call]
+	tableName := "Movies"
+	movieName := "The Big New Movie"
+	movieYear := "2016"
+
+	result, err := svc.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"Year": {
+				N: aws.String(movieYear),
+			},
+			"Title": {
+				S: aws.String(movieName),
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("Got error calling GetItem: %s", err)
+	}
+
+	item := Item{}
+
+	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	fmt.Println("Found item:")
+	fmt.Println("Year:  ", item.Year)
+	fmt.Println("Title: ", item.Title)
+	fmt.Println("Plot:  ", item.Plot)
+	fmt.Println("Rating:", item.Rating)
+	// snippet-end:[dynamodb.go.read_item.unmarshall]
+}
+
+// snippet-end:[dynamodb.go.read_item]
